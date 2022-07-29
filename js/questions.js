4987e58eaeff6c91c10c424cff011f923e52eef6
petra-b <73248692+petra-b@users.noreply.github.com>
2022-07-29 10:21:00 +0100

    question added

--- js/questions.js
+++ js/questions.js
@@ -80,13 +80,13 @@ let questions = [
   },
   {
     numb: 8,
-    question: "Your Question is Here",
-    answer: "Correct answer of the question is here",
+    question: "What fabric uses the least amount of water in its production?",
+    answer: "Recycled PET Polyester",
     options: [
-      "Option 1",
-      "option 2",
-      "option 3",
-      "option 4"
+      "Cotton",
+      "100% Organic cotton",
+      "Polyester",
+      "Recycled PET Polyester"
     ]
   },
   {
